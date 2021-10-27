15e85d207d4066da2b92c2b459fd79b99a9cb891
deepak k <deepakajay77@gmail.com>
2021-10-27 21:30:21 +0530

    User and Admin file created, User and Admin Routes defined, Page created for adding products for admin

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require('./routes/user');
+var adminRouter = require('./routes/admin');
 var hbs=require("express-handlebars")
 var app = express();
+var fileUpload=require("express-fileupload")
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,9 +20,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload())
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', userRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
